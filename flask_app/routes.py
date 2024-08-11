83de61cd46a9f888033f7abb87d3e72f19188a4f
naamashiponi <naamashiponi11@gmail.com>
2024-08-11 15:57:47 +0300

    add style to log

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -6,17 +6,6 @@ from fairpyx.algorithms.course_match.reduce_undersubscription import logger as r
 import logging
 from io import StringIO
 
-log_stream = StringIO()    
-
-ACEEI_logger.setLevel(logging.INFO)
-ACEEI_logger.addHandler(logging.StreamHandler(log_stream))
-
-remove_oversubscription_logger.setLevel(logging.INFO)
-remove_oversubscription_logger.addHandler(logging.StreamHandler(log_stream))
-
-reduce_undersubscription_logger.setLevel(logging.INFO)
-reduce_undersubscription_logger.addHandler(logging.StreamHandler(log_stream))
-
 from flask import Flask, render_template, jsonify
 from flask_app import app
 from flask_app.google_sheets import get_google_sheets_data, update_results_google_sheets
@@ -29,6 +18,18 @@ def hello():
 @app.route('/run-algorithm', methods=['POST'])
 def run_algorithm():
     try:
+        
+        log_stream = StringIO()    
+
+        ACEEI_logger.setLevel(logging.INFO)
+        ACEEI_logger.addHandler(logging.StreamHandler(log_stream))
+
+        remove_oversubscription_logger.setLevel(logging.INFO)
+        remove_oversubscription_logger.addHandler(logging.StreamHandler(log_stream))
+
+        reduce_undersubscription_logger.setLevel(logging.INFO)
+        reduce_undersubscription_logger.addHandler(logging.StreamHandler(log_stream))
+
         # Get data from Google Sheets
         item_capacities, item_conflicts, valuations, agent_conflicts, agent_capacities, agent_budgets = get_google_sheets_data()
      
@@ -38,6 +39,7 @@ def run_algorithm():
         allocation = fairpyx.divide(algorithm=fairpyx.algorithms.course_match.main_course_match.course_match_algorithm, instance=instance, budget=agent_budgets,time=5)
 
         update_results_google_sheets(allocation,agent_budgets)
+        
         print(log_stream.getvalue())
         
 
