fbdcf7f8a3a2f46977f9b2ecc1bc060fdcf5fda9
naamashiponi <naamashiponi11@gmail.com>
2024-08-11 16:03:15 +0300

    add maun fun to app.py

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ import dotenv ,os
 dotenv.load_dotenv()
 
 from flask_app import app
-
-app.run(host="0.0.0.0",debug=False, port=os.getenv("FLASK_RUN_PORT"))
-
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=False, port=os.getenv("FLASK_RUN_PORT"))
